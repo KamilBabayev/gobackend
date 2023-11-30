6d9a07127d84b97c4a72adf0c7f28efbe6bd717c
Kamil Babayev <kamilyunis@gmail.com>
2023-11-30 21:27:04 +0400

    Refactor: create custom router implmenting Handler interface and ass to ListenAndServe Method as router

--- main.go
+++ main.go
@@ -5,19 +5,9 @@ import (
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> welcome to my app home page</h1>")
-}
+type Router struct{}
 
-func userHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>loading users list...</h1>")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Our Contact info ...</h1>")
-}
-
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
@@ -30,9 +20,21 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1> welcome to my app home page</h1>")
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>loading users list...</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1> Our Contact info ...</h1>")
+}
+
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
 
 	fmt.Println("Listening on port 3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
