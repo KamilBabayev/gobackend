72b7225f52d1fcf98c2cff4ecbb771ed7c59e68c
Kamil Babayev <kamilyunis@gmail.com>
2023-12-02 01:16:03 +0400

    added faq handler

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		userHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
@@ -30,6 +32,17 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1> Our Contact info ...</h1>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+	<li><b>Is there a free version?</b> Yes! We offer a free trial for 30 days on any paid plan</li>
+	<li><b>What are your support hours?</b>We have support staff answering emails 24/7, though reponse times may be a bit slower on weekends</li>
+	<li><b>How do I contact support?</b>Email us - <a href="mailto:support@mail.com">support@mail.com</a></li>
+	</ul>
+	`)
 }
 
 func main() {
